c8ddcbb79ff1c5f94cf84c4a76aa42fb8593ef19
Bruno Figueiredo <bfigueiredo@odoogap.com>
2025-02-11 12:13:13 +0000

    [IMP] Starting the Migration of the "payment_paypal_vsf" Module

--- payment_paypal_vsf/__manifest__.py
+++ payment_paypal_vsf/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright 2023 ERPGAP/PROMPTEQUATION LDA
+# Copyright 2025 ERPGAP/PROMPTEQUATION LDA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     # Application Information
     'name': 'PayPal Payment Provider to VSF',
     'category': 'Accounting/Payment Acquirers',
-    'version': '16.0.1.0.0',
+    'version': '17.0.1.0.0',
     'summary': 'Paypal Payment Acquirer: Adapting PayPal to VSF',
 
     # Author
